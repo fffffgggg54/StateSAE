1fc17171f2fa14fd94961c2995963cb2f1a80e5a
Fredo Guan <fredo.guan@hotmail.com>
2025-03-06 16:57:36 -0700

    Update train_rwkv_residual_v100.py

--- train_rwkv_residual_v100.py
+++ train_rwkv_residual_v100.py
@@ -321,7 +321,7 @@ start_time = time.time()
 #saeList = [TopKSAE(768, 32768, k=8192, device = torch.device('cpu')) for i in range(24)]
 #denseSaeList = [DenseTopKSAE(saeList[i:i + 3]).train().to(available_gpus[d]) for d, i in enumerate(range(0, 24, 3))]
 
-baseSae = TopKMLPSAE(768, 32768, 2048, k=8192, device = torch.device('cpu'))
+baseSae = TopKMLPSAE(768, 32768, 3072, k=8192, device = torch.device('cpu'))
 saeList = [copy.deepcopy(baseSae) for i in range(24)]
 denseSaeList = [DenseTopKMLPSAE(saeList[i:i + 3]).train().to(available_gpus[d]) for d, i in enumerate(range(0, 24, 3))]
 
