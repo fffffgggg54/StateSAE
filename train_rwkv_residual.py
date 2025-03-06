662894b8abe8cb3c34e3ecffb428080e2ce1866e
Fredo Guan <fredo.guan@hotmail.com>
2025-03-05 18:04:29 -0700

    Update train_rwkv_residual.py

--- train_rwkv_residual.py
+++ train_rwkv_residual.py
@@ -294,7 +294,7 @@ while(1):
         if opt_steps % steps_per_printout == 0:
             # print training info
             print(f'tokens: {opt_steps * batch_size * grad_accum_epochs}, mse loss: {loss.cpu()}, avg step time: {(time.time() - start_time) / steps_per_printout}, tps: {(batch_size * grad_accum_epochs * steps_per_printout)/(time.time() - start_time)}')
-            print(f'dead features: {(sae.act_ema < 1e-3).sum()}
+            print(f'dead features: {(sae.act_ema < 1e-3).sum()}')
             start_time = time.time()
 
             
